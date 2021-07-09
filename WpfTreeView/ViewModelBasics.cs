aac709f12071bd9a49d23a516640de56391cc1dc
NetanelElimelech <44609836+NetanelElimelech@users.noreply.github.com>
2021-07-09 18:36:12 +0300

    continue learning

--- WpfTreeView/ViewModelBasics.cs
+++ WpfTreeView/ViewModelBasics.cs
@@ -5,10 +5,27 @@ namespace WpfTreeView
 {
     public class ViewModelBasics : INotifyPropertyChanged
     {
+        private string mTest;
+
         public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
 
-        public string Test { get; set; } = "My property";
-        
+        public string Test
+        {
+            get
+            {
+                return mTest;
+            }
+
+            set
+            {
+                if (mTest == value)
+                    return;
+
+                mTest = value;
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(Test)));
+            }
+        }
+
         public ViewModelBasics()
         {
             Task.Run(async () =>
@@ -17,9 +34,8 @@ namespace WpfTreeView
 
                 while (true)
                 {
-                    await Task.Delay(1000);
+                    await Task.Delay(200);
                     Test = i++.ToString();
-                    PropertyChanged(this, new PropertyChangedEventArgs(nameof(Test)));
                 }
             });
         }
